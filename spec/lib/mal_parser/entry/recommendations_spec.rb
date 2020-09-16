39c314caa595cfa92cd668455208a169099da4d9
Andrey Sidorov <takandar@gmail.com>
2020-09-16 23:46:22 +0300

    actualize recommendations spec

--- spec/lib/mal_parser/entry/recommendations_spec.rb
+++ spec/lib/mal_parser/entry/recommendations_spec.rb
@@ -8,25 +8,28 @@ describe MalParser::Entry::Recommendations do
 
     it do
       is_expected.to eq [
-        {
-          id: 28_735,
-          type: :anime
-        }, {
-          id: 16_662,
-          type: :anime
-        }, {
-          id: 31_771,
-          type: :anime
-        }, {
-          id: 13_673,
-          type: :anime
-        }, {
-          id: 31_953,
-          type: :anime
-        }, {
-          id: 25_835,
-          type: :anime
-        }
+        { id: 28735, type: :anime },
+        { id: 31646, type: :anime },
+        { id: 25835, type: :anime },
+        { id: 16662, type: :anime },
+        { id: 22789, type: :anime },
+        { id: 31771, type: :anime },
+        { id: 13673, type: :anime },
+        { id: 32949, type: :anime },
+        { id: 36317, type: :anime },
+        { id: 12189, type: :anime },
+        { id: 40513, type: :anime },
+        { id: 16, type: :anime },
+        { id: 31953, type: :anime },
+        { id: 37965, type: :anime },
+        { id: 34984, type: :anime },
+        { id: 12531, type: :anime },
+        { id: 10800, type: :anime },
+        { id: 33352, type: :anime },
+        { id: 23847, type: :anime },
+        { id: 39468, type: :anime },
+        { id: 457, type: :anime },
+        { id: 36653, type: :anime }
       ]
     end
 
